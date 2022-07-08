15271fe0c2765848ac3977d5dd0ea265ee2148d2
zaid <xaidmetamorphos@gmail.com>
2022-07-08 11:04:24 +0530

    first commit

---
+++ demo_flask_app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    name = "Python Flask framework"
+    task = f"Create a web application using {name}"
+    return render_template('index.html',nm=name,task=task)
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
