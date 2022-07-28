5e628a0bd6cb0ade758e104a93a91e2c1ef4d861
zaidkamil <xaidmetamorphos@gmail.com>
2022-07-28 11:17:30 +0530

    ajax is easy

--- demo_flask_app/app.py
+++ demo_flask_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
@@ -22,8 +22,25 @@ def design_basics():
 
 @app.route('/form1',methods=['GET','POST'])
 def form1_handler():
-    
     return render_template('form_1.html')
+
+@app.route('/form1_ajax',methods=['POST'])
+def form1_ajax_handler():
+    name = request.form.get('fullname')
+    email = request.form.get('email')
+    college = request.form.get('college')
+    password = request.form.get('password')
+    cpassword = request.form.get('cpassword')
+    return jsonify({'status':'success'})
+
+@app.route('/form2', methods=['GET','POST'])
+def form2_handler():
+    if request.method == "POST":
+        print(request.form.keys())
+        name = request.form.get('name')
+        city = request.form.get('city')
+        print("We got", name, city)
+    return render_template('form_2.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
